30fdf7700533f36b5e53a2d81f43320162b7beb2
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2025-03-23 03:50:36 -0500

    feat: variables and data types

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 22;
+        double salary = 10.0;
+        boolean flag = true;
+        char aChar = 'T';
+        String username = "alenj0x1";
+        long isLong = 12;
+
+        System.out.println(username);
+        System.out.println(isLong);
+        System.out.println(salary);
+    }
+}
